dcbc95c8be4864b121955df183be48d14a330b9f
Satyajeetsinh <zalasatyajeet@gmail.com>
2021-01-30 12:25:01 +0530

    search screen fixed

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import './other_profiles.dart';
+
 class SearchScreen extends StatefulWidget {
   static const routeName = '/search-screen';
   @override
@@ -8,9 +10,11 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  String name;
+  String name = 'test';
   String userId;
   String displayUserName;
+  bool search = false;
+  String email;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +25,7 @@ class _SearchScreenState extends State<SearchScreen> {
           onSubmitted: (val) {
             setState(() {
               name = val;
+              search = true;
             });
           },
           style: TextStyle(fontSize: 18, color: Colors.white),
@@ -36,45 +41,97 @@ class _SearchScreenState extends State<SearchScreen> {
           ),
         ),
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection('Users')
-            .where('username', isEqualTo: name)
-            .where('userId')
-            .snapshots(),
-        builder: (context, snapshot) {
-          var username = snapshot.data.documents.map<String>(
-            (doc) {
-              displayUserName = doc['username'].toString();
-              return doc['username'].toString();
-            },
-          );
-          var id = snapshot.data.documents.map<String>(
-            (id) {
-              userId = id['userId'].toString();
-              return id['userId'].toString();
-            },
-          );
+      body: search == false
+          ? Center(
+              child: Text('Search'),
+            )
+          : StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection('Users')
+                  .where('username', isEqualTo: name)
+                  .where('userId')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.data == null) return CircularProgressIndicator();
 
-          if (!snapshot.hasData) {
-            return Text('No data found');
-          }
-          return Center(
-            child: ListView(
-              children: <Widget>[
-                username.toString() == null ||
-                        username.toString() == ('()') ||
-                        id.toString() == null ||
-                        id.toString() == ('()')
-                    ? Text('User not available')
-                    : Text(displayUserName != null || userId != null
-                        ? displayUserName + ' ' + userId
-                        : 'Searching'),
-              ],
+                var username = snapshot.data.documents.map<String>(
+                  (doc) {
+                    displayUserName = doc['username'].toString();
+                    return displayUserName;
+                  },
+                );
+
+                var id = snapshot.data.documents.map<String>(
+                  (id) {
+                    userId = id['userId'].toString();
+                    return userId;
+                  },
+                );
+
+                var emailId = snapshot.data.documents.map<String>(
+                  (emailId) {
+                    email = emailId['email'].toString();
+                    return email;
+                  },
+                );
+
+                if (username.toString() == null ||
+                    username.toString() == ('()') ||
+                    id.toString() == null ||
+                    id.toString() == ('()') ||
+                    email.toString() == null ||
+                    email.toString() == ('()'))
+                  return Center(
+                    child: Text(
+                      'User not available',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  );
+
+                return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (ctx, index) {
+                    return Column(
+                      children: <Widget>[
+                        Card(
+                          key: ValueKey(UniqueKey),
+                          elevation: 5,
+                          child: ListTile(
+                            leading: Icon(Icons.person),
+                            title: Text(displayUserName),
+                            subtitle: Text(email),
+                            trailing: FlatButton(
+                              child: Text(
+                                'Profile',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  side: BorderSide(
+                                      color: Theme.of(context).accentColor)),
+                              onPressed: () {
+                                Navigator.of(context).pushNamed(
+                                  OtherProfileScreen.routeName,
+                                  arguments: {
+                                    'id': userId,
+                                    'name': displayUserName,
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
